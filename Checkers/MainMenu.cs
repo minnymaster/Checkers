0f16ae8e6904ada6b4af532d1c76100d2983442f
Даниил <dpbolotov@edu.hse.ru>
2023-06-12 00:31:47 +0500

    Добавьте файлы проекта.

---
+++ Checkers/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Checkers
+{
+    public partial class MainMenu : Form
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
